adce4ee492996b8cc073c5696375b6bc8d4756a1
xiaoming <2604863701@qq.com>
2022-06-26 10:49:17 +0800

    NumberPadSection 功能 work in progress

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -9,39 +9,16 @@ import {TagsSection} from './Money/TagsSection';
 const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;
-`
+`;
 
 function Money() {
   return (
-    <MyLayout >
-      <TagsSection />
-      <NoteSection />
+    <MyLayout>
+      <TagsSection/>
+      <NoteSection/>
+      <CategorySection/>
+      <NumberPadSection/>
 
-      <CategorySection />
-
-      <NumberPadSection>
-        <div className='output'>100</div>
-        <div className='Pad clearfix'>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>C</button>
-
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>CE</button>
-
-
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className='ok'>OK</button>
-          <button className='zero'>0</button>
-          <button className='dot'>.</button>
-
-        </div>
-      </NumberPadSection>
     </MyLayout>
   );
 }
--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const NumberPadSection = styled.section`
+const Wrapper = styled.section`
 display: flex;
 flex-direction: column;
   > .output{
@@ -52,4 +53,66 @@ flex-direction: column;
 
 `;
 
-export {NumberPadSection}
+const NumberPadSection: React.FC = () => {
+  const [output, setOutput] = useState('0');
+  const onClickButtonWrapper = (e: React.MouseEvent) => {
+    const text = (e.target as HTMLButtonElement).textContent;
+    if (text === null) return;
+    switch (text) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        if (output === '0') {
+          setOutput(text);
+        } else {
+          setOutput(output + text);
+        }
+        break;
+      case 'C':
+        console.log('清空');
+        break;
+      case 'CE':
+        console.log('删除');
+        break;
+      case 'OK':
+        console.log('OK');
+        break;
+      case '.':
+        console.log('点');
+        break;
+    }
+  };
+
+  return (
+    <Wrapper>
+      <div className='output'>{output}</div>
+      <div className='Pad clearfix' onClick={onClickButtonWrapper}>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>C</button>
+
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>CE</button>
+
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className='ok'>OK</button>
+        <button className='zero'>0</button>
+        <button className='dot'>.</button>
+      </div>
+    </Wrapper>
+  );
+};
+
+export {NumberPadSection};
