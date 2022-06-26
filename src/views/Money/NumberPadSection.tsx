41c32b994a05604cc389552f16e80c04ad11fda4
xiaoming <2604863701@qq.com>
2022-06-26 11:51:07 +0800

    NumberPadSection 功能 优化

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,57 +1,8 @@
-import styled from 'styled-components';
 import React, {useState} from 'react';
+import { Wrapper } from './NumberPadSection/Wrapper';
+import {generageOutput} from './NumberPadSection/generateOutput';
 
-const Wrapper = styled.section`
-display: flex;
-flex-direction: column;
-  > .output{
-    background-color: white;
-    font-size: 36px;
-    line-height: 72px;
-    text-align: right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
-                inset 0 5px 5px -5px rgba(0,0,0,0.25);
-  }
-  > .Pad{
-    > button{
-      float:left ;
-      width: 25%;
-      height: 64px;
-      font-size:18px;
-      border:none;
-      &.ok{
-        float: right;
-        height:128px;
-      }
-      &.zero{
-      width: 50%;
-      }
-      &:nth-child(1){
-        background-color: #8afcc4;
-      }
-      &:nth-child(2),&:nth-child(5){
-        background-color:#5cfabd;
-      }
-      &:nth-child(3),&:nth-child(6),&:nth-child(9){
-        background-color:#45f8ad;
-      }
-      &:nth-child(4),&:nth-child(7),&:nth-child(10){
-        background-color:#27faa3;
-      }
-      &:nth-child(8),&:nth-child(11),&:nth-child(13){
-        background-color:#19f8a2;
-      }
-       &:nth-child(12){
-        background-color:#00db8e;
-      }
-      &:nth-child(14){
-        background-color:#24e9a4;
-      }
-    }
-  }
 
-`;
 
 const NumberPadSection: React.FC = () => {
   const setOutput = (output:string)=>{
@@ -66,43 +17,12 @@ const NumberPadSection: React.FC = () => {
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (text === null) return;
-    switch (text) {
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if (output === '0') {
-          setOutput(text);
-        } else {
-          setOutput(output + text);
-        }
-        break;
-      case 'C':
-        setOutput('')
-        console.log('清空');
-        break;
-      case 'CE':
-        if(output.length ===1){
-          setOutput('')
-        }else{
-          setOutput(output.slice(0,-1))
-        }
-        console.log('删除');
-        break;
-      case 'OK':
-        console.log('OK');
-        break;
-      case '.':
-        console.log('点');
-        if(output.indexOf('.') >= 0 ){return;}
-        setOutput(output+ '.')
-        break;
+    // TODO
+    if(text === 'OK'){return ;}
+
+    if('0123456789.'.split('').concat(['C','CE']).indexOf(text)>=0){
+
+      setOutput(generageOutput(text,output))
     }
   };
 
---
+++ src/views/Money/NumberPadSection/Wrapper.tsx
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  > .output{
+    background-color: white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+    inset 0 5px 5px -5px rgba(0,0,0,0.25);
+  }
+  > .Pad{
+    > button{
+      float:left ;
+      width: 25%;
+      height: 64px;
+      font-size:18px;
+      border:none;
+      &.ok{
+        float: right;
+        height:128px;
+      }
+      &.zero{
+        width: 50%;
+      }
+      &:nth-child(1){
+        background-color: #8afcc4;
+      }
+      &:nth-child(2),&:nth-child(5){
+        background-color:#5cfabd;
+      }
+      &:nth-child(3),&:nth-child(6),&:nth-child(9){
+        background-color:#45f8ad;
+      }
+      &:nth-child(4),&:nth-child(7),&:nth-child(10){
+        background-color:#27faa3;
+      }
+      &:nth-child(8),&:nth-child(11),&:nth-child(13){
+        background-color:#19f8a2;
+      }
+      &:nth-child(12){
+        background-color:#00db8e;
+      }
+      &:nth-child(14){
+        background-color:#24e9a4;
+      }
+  }
+}
+
+`;
+
+export {Wrapper}
